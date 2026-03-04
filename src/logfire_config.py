83f2adbcf9df208651432fca191634740bc5e3c6
Kirilok <kiriloki2018@gmail.com>
2026-03-04 14:01:36 +0200

    Minor fix.

--- src/logfire_config.py
+++ src/logfire_config.py
@@ -3,10 +3,7 @@ import logfire
 
 def init_logfire():
     """Initialize Logfire instrumentation."""
-    logfire.configure(
-        service_name='starter-project',
-        console=False,
-    )
+    logfire.configure()
     logfire.instrument_mcp()
     logfire.instrument_pydantic_ai()
     logfire.instrument_httpx(capture_all=True)
